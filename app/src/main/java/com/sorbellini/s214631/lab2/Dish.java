5415af1597c081a40ce9a592a150cae0c2fb25b9
Eugenio <eugenio.sorbellini@gmail.com>
2016-04-12 15:32:07 +0200

    fixed class dish
    fixed class dish

--- app/src/main/java/com/sorbellini/s214631/lab2/Dish.java
+++ app/src/main/java/com/sorbellini/s214631/lab2/Dish.java
@@ -1,36 +1,25 @@
 package com.sorbellini.s214631.lab2;
 
 import android.graphics.Bitmap;
-<<<<<<< HEAD
 import android.os.Parcel;
 import android.os.Parcelable;
-=======
 import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 
 import java.io.Serializable;
 import java.net.URI;
->>>>>>> eugenio-reservations
 
 /**
  * Created by eugeniosorbellini on 01/04/16.
  */
-<<<<<<< HEAD
-public class Dish implements Parcelable {
-    String name;
-    float price;
-    Bitmap photo;
-    String description;
-    int availability;
-=======
+
 public class Dish implements Parcelable{
     private double price;
     private String photo;
     private String name;
     private String description;
     private int availability;
->>>>>>> eugenio-reservations
 
     //constructor
     public Dish(){
@@ -48,20 +37,15 @@ public class Dish implements Parcelable{
     public String getName() { return this.name; }
     public String getDescription() { return this.description; }
     public int getAvailability() { return this.availability; }
-    public String getName() { return this.name; }
 
     //setter
     public void setPrice(double price) { this.price =price; }
     public void setPhoto(String photo) { this.photo = photo; }
-    public void setName(String title) { this.name = title; }
     public void setDescription(String description) { this.description = description; }
-    public void setName(String Name) { this.name = name; }
+    public void setName(String name) { this.name = name; }
     public void setAvailability(int availability) { this.availability = availability; }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> eugenio-reservations
     @Override
     public int describeContents(){
         return 0;
@@ -69,24 +53,15 @@ public class Dish implements Parcelable{
 
     @Override
     public void writeToParcel(Parcel dest, int flags){
-<<<<<<< HEAD
-        dest.writeFloat(this.price);
-        this.photo.writeToParcel(dest, 0);
-=======
         dest.writeDouble(this.price);
         dest.writeString(this.photo);
         dest.writeString(this.name);
->>>>>>> eugenio-reservations
         dest.writeString(this.description);
         dest.writeInt(this.availability);
     }
 
     //Creator
-<<<<<<< HEAD
-    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
-=======
     public static final Parcelable.Creator<Dish> CREATOR = new Parcelable.Creator<Dish>() {
->>>>>>> eugenio-reservations
         public Dish createFromParcel(Parcel in) {
             return new Dish(in);
         }
@@ -98,15 +73,6 @@ public class Dish implements Parcelable{
 
     //De-parcel object
     public Dish(Parcel in){
-<<<<<<< HEAD
-        this.price = in.readFloat();
-        this.photo = Bitmap.CREATOR.createFromParcel(in);
-        this.description = in.readString();
-        this.price = in.readInt();
-    }
-
-}
-=======
         this.price = in.readDouble();
         this.photo = in.readString();
         this.name = in.readString();
@@ -115,4 +81,3 @@ public class Dish implements Parcelable{
     }
 
 }
->>>>>>> eugenio-reservations
